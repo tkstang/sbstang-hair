d43337aad3ab4167128748a2b3dbbbda9432996a
Thomas Stang <tstang@mbpr-tstang-sf.sugarinc.com>
2019-03-19 19:22:43 -0700

    Implement mobile nav button and overlay

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,58 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import { Media } from 'react-breakpoints'
 
 import Navigation from "components/navigation"
 import Footer from "components/footer"
 import styles from "styles/layout.module.scss"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children }) => {
+  const [navActive, toggleNavState] = useState(false);
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <div className={styles.layoutContainer}>
-          <div className={styles.navContainer}>
-            <div className={styles.navigation}>
-              <Navigation />
+      `}
+      render={data => (
+        <>
+          <div className={styles.layoutContainer}>
+            <div className={styles.navContainer}>
+              <Media>
+                {({ breakpoints, currentBreakpoint }) => (
+                  breakpoints[currentBreakpoint] < breakpoints.lg ? (
+                    <div className={`${styles.navButtonCircle} ${navActive ? styles.active : ''}`}
+                      onClick={() => toggleNavState(!navActive)}>
+                      <div className={`${styles.navButton} ${navActive ? styles.active : ''}`}>
+                      <span className={styles.top} />
+                      <span className={styles.middle} />
+                      <span className={styles.bottom} />
+                      </div>
+                    </div>
+                  ) : null
+                )}
+              </Media>
+              <div className={`${styles.navigation} ${navActive ? styles.open : ''}`}>
+                <Navigation active={navActive} />
+              </div>
+            </div>
+            <main className={styles.content}>{children}</main>
+            <div className={styles.footer}>
+              <Footer />
             </div>
           </div>
-          <main className={styles.content}>{children}</main>
-          <div className={styles.footer}>
-            <Footer />
-          </div>
-        </div>
-      </>
-    )}
-  />
-)
+        </>
+      )}
+    />
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
