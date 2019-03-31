94d43ad8dd4d9ef7e295dcac87646d7826d3c727
Thomas Stang <tstang@mbpr-tstang-sf.sugarinc.com>
2019-03-31 12:54:15 -0700

    Style hero for mobile device innerHeight
    Minor hero button tweaks

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'gatsby-image'
 import PropTypes from 'prop-types'
+
 import styles from 'styles/hero.module.scss'
 import Button from 'components/button'
 import ImageCarousel from 'components/image-carousel'
 
 const Hero = ({ text, buttonText, buttonLink, image }) => {
+  const [height, setHeight] = useState(`calc(100vh - 120px)`);
+
+  useEffect(() => {
+    const handleResize = () => {
+      window.innnerHeight >= 820 ? setHeight(`calc(100vh - 120px)`) : setHeight(window.innerHeight - 120);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  });
+
   return (
     <div className={styles.heroContainer}>
       {image && (
-        <div className={styles.heroImageContainer}>
+        <div className={styles.heroImageContainer} style={{ height: height }}>
           <Image
             className={styles.heroImage}
             fluid={image.childImageSharp.fluid}
--- src/components/image-carousel.js
+++ src/components/image-carousel.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Carousel } from 'react-responsive-carousel'
+import PropTypes from 'prop-types'
 import styles from "styles/image-carousel.module.scss"
 import "styles/carousel.scss"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -9,7 +10,7 @@ import image2 from "../images/umbrella-2.jpg"
 import image3 from "../images/umbrella-3.jpg"
 import image7 from "../images/umbrella-7.jpg"
 
-const ImageCarousel = () => {
+const ImageCarousel = ({ height }) => {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -22,7 +23,7 @@ const ImageCarousel = () => {
       {
         [wedding1, image2, image3, image7].map((image, i) => {
           return (
-            <div className={styles.heroImageContainer} key={`image${i}`}>
+            <div className={styles.heroImageContainer} style={{ height: height }} key={`image${i}`}>
               <img className={styles.image} src={image} alt={'hero'}/>
             </div>
           )
@@ -31,5 +32,14 @@ const ImageCarousel = () => {
     </Carousel>
   )
 }
+
+ImageCarousel.propTypes = {
+  height: PropTypes.string
+}
+
+ImageCarousel.defaultProps = {
+  height: `calc(100vh - 120px)`
+}
+
 
 export default ImageCarousel
