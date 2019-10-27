1edd1cb99aed266082a57b80bb87bd47a6676c5f
Thomas Stang <tstang@popsugar.com>
2019-10-27 08:48:12 -0700

    Update pics

--- src/components/image-carousel.js
+++ src/components/image-carousel.js
@@ -8,6 +8,8 @@ import wedding1 from "../images/leal-wedding-01.jpg"
 import jennHairspray from "../images/jenn-hairspray.jpg"
 import octShoot3 from "../images/oct-shoot-3.jpg"
 import octShoot6 from "../images/oct-shoot-6.jpg"
+import octShoot24 from "../images/oct-shoot2-4.jpg"
+import octShoot25 from "../images/oct-shoot2-5.jpg"
 
 const ImageCarousel = () => {
   const [mounted, setMounted] = useState(false);
@@ -20,7 +22,7 @@ const ImageCarousel = () => {
     mounted &&
     <Carousel className={styles.imageCarousel} showArrows={false} showThumbs={false} showStatus={false} autoPlay={true} infiniteLoop>
       {
-        [jennHairspray, octShoot3, wedding1, octShoot6].map((image, i) => {
+        [jennHairspray, octShoot24, octShoot6, wedding1, octShoot3].map((image, i) => {
           return (
             <div className={styles.heroImageContainer} key={`image${i}`}>
               <img className={styles.image} src={image} alt={'hero'}/>
