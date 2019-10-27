ebe0363422ab410826630171ccd867931b6cf985
Thomas Stang <tstang@popsugar.com>
2019-10-27 07:23:34 -0700

    Update carousel images

--- src/components/image-carousel.js
+++ src/components/image-carousel.js
@@ -5,9 +5,9 @@ import "styles/carousel.scss"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import wedding1 from "../images/leal-wedding-01.jpg"
-import image2 from "../images/umbrella-2.jpg"
-import image3 from "../images/umbrella-3.jpg"
-import image7 from "../images/umbrella-7.jpg"
+import jennHairspray from "../images/jenn-hairspray.jpg"
+import octShoot3 from "../images/oct-shoot-3.jpg"
+import octShoot6 from "../images/oct-shoot-6.jpg"
 
 const ImageCarousel = () => {
   const [mounted, setMounted] = useState(false);
@@ -20,7 +20,7 @@ const ImageCarousel = () => {
     mounted &&
     <Carousel className={styles.imageCarousel} showArrows={false} showThumbs={false} showStatus={false} autoPlay={true} infiniteLoop>
       {
-        [wedding1, image2, image3, image7].map((image, i) => {
+        [jennHairspray, octShoot3, wedding1, octShoot6].map((image, i) => {
           return (
             <div className={styles.heroImageContainer} key={`image${i}`}>
               <img className={styles.image} src={image} alt={'hero'}/>
