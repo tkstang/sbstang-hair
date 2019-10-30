d6632dd24d41be53b04cda64d4dfe65e6fc0ca7a
Thomas Stang <tstang@popsugar.com>
2019-10-29 19:28:01 -0700

    Minor design tweaks

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -8,13 +8,13 @@ const Testimonials = () => {
       reviewer: 'Erin A.'
     },
     {
+      review: `"I have never met a hair stylist who took so much pride and care in the perfection of their customer's hair.  I walked out feeling like a million bucks and my hair color looks natural and beautiful."`,
+      reviewer: 'Kaede H.'
+    },
+    {
       review: `"She is a true perfectionist and I recommend her for any hair service, especially color.  I've been going to her for years, and she has yet to disappoint."`,
       reviewer: 'Emily B.'
     },
-    {
-      review: `"I have never met a hair stylist who took so much pride and care in the perfection of their customer's hair.  I walked out feeling like a million bucks and my hair color looks natural and beautiful."`,
-      reviewer: 'Kaede H.'
-    }
   ];
 
   // For smaller screens this will shift between the three reviews
