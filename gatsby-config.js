c8ecfcb367ff4b7d97636b4bc78461d20460ea61
Thomas Stang <tstang@popsugar.com>
2020-01-26 11:10:06 -0800

    Add gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     instagramUsername: `sbstanghair`
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
